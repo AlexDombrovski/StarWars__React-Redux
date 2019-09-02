5f176d9d91259d5119c4ea033e8a858803d5a9a1
Alex Dombrovski <ad19702004@mail.ru>
2019-09-02 13:56:55 +0700

    Create page-people

--- src/components/home/home-page.jsx
+++ src/components/home/home-page.jsx
@@ -1,29 +1,25 @@
 import React from 'react';
 import RandomPlanet from '../random-planet';
-import PersonDetails from '../person-details';
-import PersonList from '../person-list';
+// import PersonDetails from '../person-details';
+// import PersonList from '../person-list';
 
 export default class HomePage extends React.Component {
 
-    state = {
-      personData: {}
-  }
+    
 
-  call = (data) => {
-      this.setState({ personData: data });
-  }
+  // call = (data) => {
+  //     this.setState({ personData: data });
+  // }
 
   render() {
-
-    const { personData } = this.state;
-    
+  
     return(
       <div className="container">
         <RandomPlanet />
-        <div className="container persons">
+        {/* <div className="container persons">
           <PersonList call={this.call} />
           {personData.name && <PersonDetails personData={personData} />}
-        </div>                   
+        </div>                    */}
       </div>
     );
   }
--- src/components/page-people/page-people.jsx
+++ src/components/page-people/page-people.jsx
@@ -1,16 +1,49 @@
 import React from 'react';
 
+import SwapiService from '../../services/swapi-service.js';
+
 import './page-people.css';
 
 export default class PagePeople extends React.Component {
 
+  swapiService = new SwapiService();
+
+  state = {
+    allPeople: [],
+    personData: {}
+  }
+
+  async setStatePeople() {
+    const peoples = await this.swapiService.getAllPeople();
+    this.setState({ allPeople: peoples });
+  }
+
+  componentDidMount() {
+    this.setStatePeople();
+  }
+
+  showPersonsCard = (data) => {
+    this.setState({ personData: data });
+  }
+
   render() {
+
+    const { allPeople, personData } = this.state;
+
+    const allNames = allPeople.map((item) => {
+      const { name, gender, birth_year, eye_color } = item;
+      return (
+        <li className="list-group-item" key={name} onClick={() => this.showPersonsCard({ name, gender, birthYear: birth_year, eyeColor: eye_color })}>
+          {name}
+        </li>
+      );
+    });
 
     return (
       <div className="container page-people">
         <h4 className="people-title">All persons</h4>
         <ul className="item-list list-group">
-          <li className="list-group-item">Name of person</li>
+          {allNames}
         </ul>
       </div>
     );
